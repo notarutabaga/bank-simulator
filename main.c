603080050e0bdc65cee28ac5323843f5e24511d6
Abrianna <abriannalynn0027@gmail.com>
2021-10-30 15:05:15 -0400

    added transfer function and cleaned up main

--- main.c
+++ main.c
@@ -33,76 +33,82 @@ bank* initbank() {
     return mybank;
 }
 
-void displaybank(bank* mybank) {
+void displaybank(bank* mybank, int day) {
+    system("cls");
+    printf("---------------- day %d ----------------\n", day);
     for(int i = 0; i < mybank->numaccounts; i++) {
         printf("| %d: %s | \ncurrent balance: $%.2f\n\n", i + 1, mybank->accounts[i]->name, mybank->accounts[i]->balance);
+    }
+    printf("-----------------------------------------\n\n");
+}
+
+int getchoice(bank* mybank) {
+    int choice;
+
+    printf("1) transfer     2) end day\n\n");
+    while (1) {
+        printf("enter choice: ");
+        scanf("%d", &choice);
+        if (choice >= 1 && choice <= 2) break;
+        else printf("invalid choice\n");
+    }
+
+    return choice;
+}
+
+void transfer(bank* mybank) {
+    if (mybank->numaccounts > 1) {    
+        int to, from;
+        float amount;
+    
+        while (1) {
+            printf("from account: ");
+            scanf("%d", &from);
+            if (from <= mybank->numaccounts) break;
+            else printf("account does not exist\n");
+        }
+
+        while (1) {
+            printf("to account: ");
+            scanf("%d", &to);
+            if (to <= mybank->numaccounts) break;
+            else printf("account does not exist\n");
+        }
+
+        while (1) {
+            printf("amount: $");
+            scanf("%f", &amount);
+            if (amount <= mybank->accounts[from - 1]->balance) break;
+            else printf("amount too high\n");
+        }
+
+        mybank->accounts[from - 1]->balance -= amount;
+        mybank->accounts[to - 1]->balance += amount;
     }
 }
 
 int main() {
     bank* mybank = initbank();
+    system("cls");
 
     float paycheck;
     int day = 1, week = 1;
-    int choice, from, to;
-    float amount;
-
-    system("cls");
+    int choice;
 
     //day loop
     while (1) {
         //printf("week %d\n", week);
-            
-        system("cls");
-        printf("---------------- day %d ----------------\n", day);
-        displaybank(mybank);
-        printf("-----------------------------------------\n\n");
+        displaybank(mybank, day);
 
         //menu loop
         while (1) {
-            printf("1) transfer     2) end day\n\n");
-            while (1) {
-                printf("enter choice: ");
-                scanf("%d", &choice);
-                if (choice >= 1 && choice <= 2) break;
-                else printf("invalid choice\n");
-            }
+            choice = getchoice(mybank);
 
-            //make a transfer
-            //move to one function pls you heathen
-            if (choice == 1) {
-                while(1) {
-                    printf("from account: ");
-                    scanf("%d", &from);
-                    if (from <= mybank->numaccounts) break;
-                    else printf("account does not exist\n");
-                }
-
-                while(1) {
-                    printf("to account: ");
-                    scanf("%d", &to);
-                    if (to <= mybank->numaccounts) break;
-                    else printf("account does not exist\n");
-                }
-
-                while(1) {
-                    printf("amount: $");
-                    scanf("%f", &amount);
-                    if (amount <= mybank->accounts[from - 1]->balance) break;
-                    else printf("amount too high\n");
-                }
-
-                mybank->accounts[from - 1]->balance -= amount;
-                mybank->accounts[to - 1]->balance += amount;
-            }
+            if (choice == 1) transfer(mybank);
 
             if (choice == 2) break;
 
-            system("cls");
-            printf("---------------- day %d ----------------\n", day);
-            displaybank(mybank);
-            printf("-----------------------------------------\n\n");
-
+            displaybank(mybank, day);
         }
 
         if (day % 7 == 0) week++;
